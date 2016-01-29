83c70faa40348152bddb2f973435426e87d67fc7
thele116 <thele116@morris.umn.edu>
2016-01-29 14:28:52 -0600

    Added clarifying comments to app.js.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// Example Module:
+// angular.module('nameOfModule', ['dependency1', 'dependency2', 'dependency3']);
+// Dependencies will be automatically injected into the module that requires them.
+
 angular.module('stdControllers', []);
 angular.module("mainApp", ['stdControllers']);
 
