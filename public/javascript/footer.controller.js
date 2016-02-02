62b9ca998934285f7da0e2a33a795650a752aae8
mitc0417 <mitc0417@morris.umn.edu>
2016-02-02 13:41:31 -0600

    added module and controller to footer.controller.js

--- public/javascript/footer.controller.js
+++ public/javascript/footer.controller.js
@@ -1,3 +1,9 @@
 /**
  * Created by mitc0417 on 2/2/16.
  */
+angular.module('stdComponents').controller('footerCtrl', function(){
+    var footer = this;
+    console.log("footer controller loaded");
+
+
+});
